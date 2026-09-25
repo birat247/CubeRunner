9cd0857a37ae865917c2ab616acb79dae1fdce1a
Birat Tripathee <birattripathee.gandaki@gmail.com>
2026-09-25 20:13:01 +0545

    Add ground materials and editable scene
    Adds two ground materials and a new 'Editable scene' (full scene YAML with camera, lights, player, and ground objects). Updates SampleScene to include a camera-follow MonoBehaviour, increase ground extents, and fix player minX/maxX ordering. Modifies FollowPlayer.cs to expose an offSet field and set the camera's z position to playerTransform.position.z + offSet each frame.

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,18 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public Transform playerTransform; // Reference to the player's transform
+    public Transform playerTransform;
+    public float offSet;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 cameraPos = transform.position;
+        cameraPos.z = playerTransform.position.z + offSet;
+        transform.position = cameraPos;
     }
 }

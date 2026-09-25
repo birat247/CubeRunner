c934e15424063966c7f3d2bcb1248e8470fe9c8e
Birat Tripathee <birattripathee.gandaki@gmail.com>
2026-09-25 16:47:53 +0545

    Add Rigidbody field and update Player in scene
    Add a public Rigidbody field to PlayerScript so the Rigidbody can be assigned from the Inspector. Update SampleScene: add a PlayerScript MonoBehaviour to the Player GameObject (with a reference to the existing Rigidbody), change the Player transform position (z set to 0), move another scene transform (position/rotation changes) and adjust a platform/ground transform (z changed to 45, scale modified). These changes wire the Player script to the Rigidbody and reposition scene objects for the updated layout.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public Rigidbody rigidbody;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {

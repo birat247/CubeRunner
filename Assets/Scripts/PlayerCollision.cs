405f44db1c7726864b70ce0fcb34473486115d14
Birat Tripathee <birattripathee.gandaki@gmail.com>
2026-09-26 21:41:51 +0545

    Enable collectible trigger collisions
    The red cube prefab was changed to a trigger so player collision checks can detect collectable objects. PlayerCollision now listens for OnTriggerEnter and destroys any object tagged as Collectables when the player overlaps it.

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public PlayerScript playerScript;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "Collectables")
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
